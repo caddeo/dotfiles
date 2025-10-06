14245c9892831d0e57a9028a2a0e354a0f8c311f
Patrick <pani@queue-it.com>
2025-10-06 11:53:02 +0200

    Added docker support

--- .config/nvim/lua/extras/extras.lua
+++ .config/nvim/lua/extras/extras.lua
@@ -3,6 +3,7 @@ return {
   -- lang
   {
     { import = "lazyvim.plugins.extras.lang.angular" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
