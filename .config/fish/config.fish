6fe6b94affdcb453b8b3bfebfd679568dbdad98f
caddeo <patrick@caddeo.dk>
2025-12-07 13:01:52 +0100

    Added mise

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -32,3 +32,5 @@ set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
 if test -f $LOCAL_CONFIG
     source $LOCAL_CONFIG
 end
+~/.local/bin/mise activate fish | source
+~/.local/bin/mise activate fish | source
