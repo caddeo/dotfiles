d48ed4ba655be36bfa476073b559cd43b4893339
caddeo <patrick@caddeo.dk>
2026-04-27 12:27:29 +0200

    Removed a dead keybind

--- .config/nvim/lua/plugins/tmux.lua
+++ .config/nvim/lua/plugins/tmux.lua
@@ -9,8 +9,6 @@ return {
           down = "<C-j>",
           up = "<C-k>",
           right = "<C-l>",
-          last_active = "<C-\\>",
-          next = "<C-Space>",
         },
       })
     end,
