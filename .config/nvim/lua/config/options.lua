bc32fe427e5ac4a75f8df7320cb04ce5ae39261b
caddeo <patrick@caddeo.dk>
2026-06-30 14:44:57 +0200

    fix(nvim): only run prettier when project has a prettier config

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 -- vim.g.lazyvim_picker = "telescope"
+
+-- Only run prettier when the project has an explicit prettier config file
+vim.g.lazyvim_prettier_needs_config = true
