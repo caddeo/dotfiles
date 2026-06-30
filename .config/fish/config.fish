1dc30ee3c7a2b9c6f9dc7027152ae28e49142094
caddeo <patrick@caddeo.dk>
2026-06-30 09:16:03 +0200

    vim bindings improvemnets

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,12 @@
 set fish_greeting
 
 fish_vi_key_bindings
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
+set fish_cursor_replace underscore
+set fish_cursor_external line
+set fish_cursor_visual block
 
 set -gx TERM xterm-256color
 set -gx TERMINAL ghostty
